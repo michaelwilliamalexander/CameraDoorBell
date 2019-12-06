729dc57012ae5fea37d7633f6b789d1f6f65a343
Ananda Kusumawardana <ananda.kusumawardana@ti.ukdw.ac.id>
2019-12-06 18:22:10 +0700

    Adding Toolbar

--- app/src/main/java/com/example/doorbellcamera/MainActivity.java
+++ app/src/main/java/com/example/doorbellcamera/MainActivity.java
@@ -3,12 +3,13 @@ package com.example.doorbellcamera;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 public class MainActivity extends AppCompatActivity {
 
     StaggeredGridLayoutManager gridLayoutManager;
-
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
