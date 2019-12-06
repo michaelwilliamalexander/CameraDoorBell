420a169ef5ba62601925acf9d8e7c3c81378dbbc
Michael William Alexander <michael.william@i.ukdw.ac.id>
2019-12-06 19:31:08 +0700

    Setting StaggeredGridManager

--- app/src/main/java/com/example/doorbellcamera/MainActivity.java
+++ app/src/main/java/com/example/doorbellcamera/MainActivity.java
@@ -1,18 +1,51 @@
 package com.example.doorbellcamera;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 public class MainActivity extends AppCompatActivity {
 
     StaggeredGridLayoutManager gridLayoutManager;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        recyclerView = findViewById(R.id.recyclerView);
+        gridLayoutManager = new StaggeredGridLayoutManager(2, LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(gridLayoutManager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_logout) {
+            Intent logout = new Intent(MainActivity.this, LoginActivity.class);
+            logout.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            logout.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(logout);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
