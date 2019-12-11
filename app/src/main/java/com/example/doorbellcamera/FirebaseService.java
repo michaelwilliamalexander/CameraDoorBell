2df41dd30b32a60d8c8a5671e0a63e16dd9e4fdb
wayanedi <wayan.sudarma@ti.ukdw.ac.id>
2019-12-12 03:54:09 +0700

    coba

---
+++ app/src/main/java/com/example/doorbellcamera/FirebaseService.java
@@ -0,0 +1,18 @@
+package com.example.doorbellcamera;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class FirebaseService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        System.out.println(remoteMessage.getData().get("title"));
+    }
+}
